fb7e666ba3aa241a58267541d24da6b286f10293
Tomasz Dzieniak <tomasz.dzieniak@to-dev.com>
2016-08-08 16:09:06 +0200

    Replace drawables with nodpi versions. Add home location to gradle.properties.

--- app/src/main/java/com/todev/samples/Constants.java
+++ app/src/main/java/com/todev/samples/Constants.java
@@ -3,8 +3,8 @@ package com.todev.samples;
 import com.google.android.gms.maps.model.LatLng;
 
 public class Constants {
-  // TODO: Put your home location here.
-  public static final double HOME_LATITUDE = 0;
-  public static final double HOME_LONGITUDE = 0;
+  // TODO: Put your home location in gradle.properties.
+  public static final double HOME_LATITUDE = BuildConfig.HOME_LATITUDE;
+  public static final double HOME_LONGITUDE = BuildConfig.HOME_LONGITUDE;
   public static final LatLng HOME_LOCATION = new LatLng(HOME_LATITUDE, HOME_LONGITUDE);
 }
